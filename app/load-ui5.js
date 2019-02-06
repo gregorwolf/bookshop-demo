c887b96d05d77222a5edfabf485357816e3c4ba1
Daniel <daniel.hutzel@sap.com>
2019-02-06 21:53:47 +0100

    .

---
+++ app/load-ui5.js
@@ -0,0 +1,48 @@
+!function(){ /* globals sap document */
+
+  var script = document.currentScript
+  var options = {
+    url: script.getAttribute('from') || 'https://sapui5.hana.ondemand.com',
+    version: script.getAttribute('version') || '1.61.1',
+  }
+
+  load ({ src: "/sapui5/sandbox.js",
+    onload: function () { loadUI5("/sapui5") },
+    onerror: function () {
+      load ({ src: "https://sapui5.hana.ondemand.com/test-resources/sap/ushell/bootstrap/sandbox.js",
+        onload: function () { loadUI5 (options.url) },
+        onerror: function () { error("Remote sandbox not found!") }
+      });
+    }
+  })
+
+
+  function loadUI5(uri) {
+    var ui5 = document.createElement('script');
+    ui5.id="sap-ui-bootstrap";
+    ui5.src=uri +'/'+ options.version + "/resources/sap-ui-core.js";
+    ui5.setAttribute("data-sap-ui-libs","sap.m, sap.ushell, sap.collaboration, sap.ui.layout");
+    ui5.setAttribute("data-sap-ui-compatVersion","edge");
+    ui5.setAttribute("data-sap-ui-theme","sap_belize");
+    ui5.setAttribute("data-sap-ui-frameOptions","allow");
+    ui5.onload = function() {
+      sap.ui.getCore().attachInit(()=> sap.ushell.Container.createRenderer().placeAt("content"));
+    };
+    ui5.onerror = function() {
+      error("SAP UI5 " + ui5.version + " not found!")
+    }
+    document.getElementsByTagName('head')[0].appendChild(ui5);
+  }
+
+
+  function load (options) {
+    var script = document.createElement('script');
+    Object.assign (script, options);
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
+
+  function error (msg) {
+    document.write("<h2>"+msg+"</h2>")
+  }
+
+}()
