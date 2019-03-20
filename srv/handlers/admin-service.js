6d753f0f6568b7a4b1b902245aa2fe2b1a2e2ac1
Christian Georgi <christian.georgi@sap.com>
2019-03-20 17:19:42 +0100

    Code cleanup

--- srv/handlers/admin-service.js
+++ srv/handlers/admin-service.js
@@ -5,17 +5,18 @@ module.exports = (srv) => {
   // calculate the netAmount for the order's items based on the books' prices
   srv.before ('PATCH', 'OrderItems', async (req) => {
     const item = req.data
+    const tx = cds.transaction(req)
     // need to read the current data, which is the drafts table
     // TODO remove dependency on draft mode
     const OrderItemsDrafts = OrderItems['@cds.persistence.name'] + '_drafts'
-    const completeItems = await req.run(SELECT.one(OrderItemsDrafts).where({ ID: item.ID }))
+    const completeItems = await tx.run(SELECT.one(OrderItemsDrafts).where({ ID: item.ID }))
     if (!completeItems || completeItems.length < 1)  req.reject(400, `No item with id ${item.ID} found`)
 
     //merge the new values into completed items
     const completeItem = Object.assign(completeItems[0], item)
 
     if (completeItem.book_ID) {
-      const books = await req.run(SELECT.one(Books).where({ ID: completeItem.book_ID }))
+      const books = await tx.run(SELECT.one(Books).where({ ID: completeItem.book_ID }))
       const book = books.length === 1 && books[0]
       if (completeItem.amount && book.price ) {
         item.netAmount = completeItem.amount * book.price
@@ -24,18 +25,11 @@ module.exports = (srv) => {
   })
 
   // on-the-fly calculate the total Order price based on the OrderItems' netAmounts
-  srv.after ('READ', 'Orders', async (orders, req) => {
-    const orderIDs = orders.map(order => order.ID)
-    const items = await req.run(SELECT.from(OrderItems, ['ID', 'parent_ID', 'netAmount']).where({ parent_ID: {in: orderIDs} }))
+  srv.after ('READ', 'Orders', computeTotal)
+  // TODO remove dependency on draft mode
+  srv.after ('EDIT', 'Orders', computeTotal)
 
-    const ordersByID = {}
-    orders.forEach(order => { order.total = 0; ordersByID[order.ID] = order })
-    items.forEach(item => {
-      ordersByID[item.parent_ID].total += item.netAmount
-    })
-  })
-
-  // some event tracing, for troubleshooting
+    // some event tracing, for troubleshooting
   if (process.env.DEBUG) {
     srv.before('*', (req) => {
       const eventToString = (req) => `${req.event}${req.target ? ' ' + req.target.name : ''}`
@@ -43,6 +37,16 @@ module.exports = (srv) => {
       req.on('succeeded', (req) => console.log(`${eventToString(req)} succeeded`))
       req.on('done', (req) => console.log(`${eventToString(req)} is done`))
     })
+  }
+
+  async function computeTotal (orders, req) {
+    const ordersByID = {}
+    orders.forEach(order => { order.total = 0; ordersByID[order.ID] = order })
+    const items = await cds.transaction(req).run(
+      SELECT.from  ( OrderItems, ['parent_ID', 'netAmount'] )
+            .where ({ parent_ID: { in: Object.keys(ordersByID) } })
+    )
+    items.forEach(item => { ordersByID[item.parent_ID].total += item.netAmount })
   }
 
 }
