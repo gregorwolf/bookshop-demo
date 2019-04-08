af9a37a24877c6c55f81f3eab3d8f57da4213513
Daniel <daniel.hutzel@sap.com>
2019-04-08 14:38:38 +0200

    removed usage of _draft tables and streamlined

--- srv/handlers/admin-service.js
+++ srv/handlers/admin-service.js
@@ -1,52 +1,37 @@
+const { DEBUG } = process.env
+
 module.exports = (srv) => {
 
-  const {Books, OrderItems} = cds.entities('AdminService')
+  const { Books, OrderItems } = srv.entities ('my.bookshop')
+
+  // on-the-fly calculate the total Order price based on the OrderItems' netAmounts
+  srv.after (['READ','EDIT'], 'Orders', async (orders, req) => {
+    const ordersByID = orders.reduce ((all,o) => { (all[o.ID] = o).total=0; return all },{})
+    return cds.transaction(req) .run (
+      SELECT.from(OrderItems) .columns ('parent_ID', 'netAmount')
+       .where ({ parent_ID: {in: Object.keys(ordersByID)} })
+    ) .then (items =>
+      items.forEach (item => ordersByID [item.parent_ID] .total += item.netAmount)
+    )
+  })
 
   // calculate the netAmount for the order's items based on the books' prices
   srv.before ('PATCH', 'OrderItems', async (req) => {
-    const item = req.data
+    const {ID,amount} = req.data; if (!amount)  return // amount not touched
     const tx = cds.transaction(req)
-    // need to read the current data, which is the drafts table
-    // TODO remove dependency on draft mode
-    const OrderItemsDrafts = OrderItems['@cds.persistence.name'] + '_drafts'
-    const completeItems = await tx.run(SELECT.one(OrderItemsDrafts).where({ ID: item.ID }))
-    if (!completeItems || completeItems.length < 1) return req.reject(400, `No item with id ${item.ID} found`)
-
-    //merge the new values into completed items
-    const completeItem = Object.assign(completeItems[0], item)
-
-    if (completeItem.book_ID) {
-      const books = await tx.run(SELECT.one(Books).where({ ID: completeItem.book_ID }))
-      const book = books.length === 1 && books[0]
-      if (completeItem.amount && book.price ) {
-        item.netAmount = completeItem.amount * book.price
-      }
-    }
+    const [item] = await tx.run (SELECT.one(req.target).where({ID}))  // FIXME: SELECT.one should return *one* entry
+    if (!item)  req.reject(400, `No order item with id ${ID} found`)
+    if (!item.book_ID)  return //> no book assigned yet
+    const [{price}] = await tx.run (SELECT.one (Books, ['price']).where({ ID: item.book_ID }))  // FIXME: SELECT.one should return *one* entry
+    req.data.netAmount = (price||0) * (amount||0)
   })
 
-  // on-the-fly calculate the total Order price based on the OrderItems' netAmounts
-  srv.after ('READ', 'Orders', computeTotal)
-  // TODO remove dependency on draft mode
-  srv.after ('EDIT', 'Orders', computeTotal)
-
     // some event tracing, for troubleshooting
-  if (process.env.DEBUG) {
-    srv.before('*', (req) => {
-      const eventToString = (req) => `${req.event}${req.target ? ' ' + req.target.name : ''}`
-      req.on('failed', (err, req) => console.log(`${eventToString(req)} failed with error ${err.message}`))
-      req.on('succeeded', (req) => console.log(`${eventToString(req)} succeeded`))
-      req.on('done', (req) => console.log(`${eventToString(req)} is done`))
-    })
-  }
-
-  async function computeTotal (orders, req) {
-    const ordersByID = {}
-    orders.forEach(order => { order.total = 0; ordersByID[order.ID] = order })
-    const items = await cds.transaction(req).run(
-      SELECT.from  ( OrderItems, ['parent_ID', 'netAmount'] )
-            .where ({ parent_ID: { in: Object.keys(ordersByID) } })
-    )
-    items.forEach(item => { ordersByID[item.parent_ID].total += item.netAmount })
-  }
+  DEBUG && srv.before('*', (req) => {
+    const eventToString = (req) => `${req.event}${req.target ? ' ' + req.target.name : ''}`
+    req.on('failed', (err, req) => console.log(`${eventToString(req)} failed with error ${err.message}`))
+    req.on('succeeded', (req) => console.log(`${eventToString(req)} succeeded`))
+    req.on('done', (req) => console.log(`${eventToString(req)} is done`))
+  })
 
 }
