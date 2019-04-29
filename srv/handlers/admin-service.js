5e41b6c2c25f777bf8e2ab7338741ed32254beb6
Ulrich Block <u.block@sap.com>
2019-04-29 17:18:42 +0200

    Validate on activate

--- srv/handlers/admin-service.js
+++ srv/handlers/admin-service.js
@@ -2,7 +2,22 @@ const { DEBUG } = process.env
 
 module.exports = (srv) => {
 
-  const { OrderItems } = srv.entities ('my.bookshop')
+  const { Books, OrderItems } = srv.entities ('my.bookshop')
+
+  // Check all amounts against stock
+  srv.before (['CREATE', 'UPDATE'], 'Orders', async (req) => {// TODO: change to SAVE once implemented as alias for CREATE and UPDATE
+    const cqn = SELECT.from(Books).columns('title')
+
+    for (const {amount, book_ID} of req.data.Items) {
+      cqn.or({ID: book_ID, and: {stock: {'<': amount}}})
+    }
+
+    const tx = cds.transaction(req)
+
+    for (const {title} of await tx.run(cqn)) {
+      req.error(400, `Amount is greater than stock for book ${title}`, 'OrderItems/amount')
+    }
+  })
 
   // on-the-fly calculate the total Order price based on the OrderItems' netAmounts
   srv.after (['READ','EDIT'], 'Orders', async (orders, req) => {
