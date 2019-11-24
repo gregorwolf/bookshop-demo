d92e91a9eb12faace81714a5c0da669a67d2984b
Gregor Wolf <gregor.wolf@gmail.com>
2019-11-24 19:55:03 +0100

    Adjust v2 server to match documentation

--- v2server.js
+++ v2server.js
@@ -1,11 +1,37 @@
-const proxy = require('@sap/cds-odata-v2-adapter-proxy')
-const cds = require('@sap/cds')
-const express = require('express')
-const PORT = process.env.PORT || 4004
+// load modules
+const express = require('express');
+const cds = require('@sap/cds');
+const proxy = require('@sap/cds-odata-v2-adapter-proxy');
 
-var app = express()
-app.use('/', express.static('app/'))
+// config
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 4004;
 
-cds.connect.to("db");
-cds.serve('all').in(app)
-app.use(proxy({ port: PORT })).listen(PORT)
+(async () => {
+  // create new app
+  const app = express();
+  app.use('/', express.static('app/'))
+
+  app.get('/', function(req, res) {
+    res.redirect('/fiori.html')
+  });
+
+  // serve odata v4
+  await cds
+    .connect('db') // ensure database is connected!
+    .serve('all')
+    .in(app);
+
+  // serve odata v2
+  process.env.XS_APP_LOG_LEVEL = 'none'; // suppress debug logs
+  app.use(proxy({
+    // app
+    path: 'v2',
+    // target
+    port: port
+  }));
+
+  // start server
+  const server = app.listen(port, host, () => console.info(`app is listing at ${port}`));
+  server.on('error', error => console.error(error.stack));
+})();
