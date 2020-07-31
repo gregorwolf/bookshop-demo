96c1f07eb19e52fa9b09407ef829236e396a6991
Gregor Wolf <gregor.wolf@gmail.com>
2020-07-31 09:28:49 +0200

    Add app that works with SAPUI5 latest

---
+++ app/webapp/v2/booksUI5latest/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("v2.booksUI5latest.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
