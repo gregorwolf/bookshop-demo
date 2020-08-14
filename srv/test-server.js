7900be4553493b7292b8b2fa16bf6b537a407d76
Gregor Wolf <gregor.wolf@gmail.com>
2020-08-14 18:55:02 +0200

    Test API Acces with OAuth 2.0 Client Credentials

---
+++ srv/test-server.js
@@ -0,0 +1,69 @@
+// Sample Code from
+// https://blogs.sap.com/2020/06/02/how-to-call-protected-app-from-external-app-as-external-user-with-scope/
+const express = require('express');
+const passport = require('passport');
+const xsenv = require('@sap/xsenv');
+xsenv.loadEnv();
+const JWTStrategy = require('@sap/xssec').JWTStrategy;
+
+//configure passport
+const services = xsenv.getServices({ xsuaa: { tags: "xsuaa" }})
+const xsuaaCredentials = services.xsuaa
+const jwtStrategy = new JWTStrategy(xsuaaCredentials)
+
+// configure express server with authentication middleware
+passport.use(jwtStrategy);
+const app = express();
+
+// Middleware to read JWT sent by client
+function jwtLogger(req, res, next) {
+   console.log('===> Decoding auth header' )
+   const jwtToken = readJwt(req)
+   if(jwtToken){
+      console.log('===> JWT: scopes: ' + jwtToken.scope);
+      console.log('===> JWT: client_id: ' + jwtToken.client_id);
+      console.log('===> JWT: user: ' + jwtToken.user_name);
+   }
+
+   next()
+}
+
+app.use(jwtLogger)
+app.use(passport.initialize());
+app.use(passport.authenticate('JWT', { session: false }));
+
+// app endpoint with authorization check
+app.get('/api/getData', function(req, res){
+   console.log('===> Endpoint has been reached. Now checking authorization')
+   const MY_SCOPE = xsuaaCredentials.xsappname + '.admin'// scope name copied from xs-security.json
+   if(req.authInfo.checkScope(MY_SCOPE)){
+      res.send('The endpoint was properly called, role available, delivering data');
+   }else{
+      const jwtToken = readJwt(req)
+      const availableScopes = jwtToken ? jwtToken.scope : {}
+   
+      return res.status(403).json({
+         error: 'Unauthorized',
+         message: `Missing required role: <admin>. Available scopes: ${availableScopes}`
+     });
+   }
+});
+
+const readJwt = function(req){
+   const authHeader = req.headers.authorization;
+   if (authHeader){
+      const theJwtToken = authHeader.substring(7);
+      if(theJwtToken){
+         const jwtBase64Encoded = theJwtToken.split('.')[1];
+         if(jwtBase64Encoded){
+            const jwtDecoded = Buffer.from(jwtBase64Encoded, 'base64').toString('ascii');
+            return JSON.parse(jwtDecoded);           
+         }
+      }
+   }
+}
+
+// start server
+app.listen(process.env.PORT || 4004, () => {
+   console.log('Server running...')
+})
--- srv/v2server.js
+++ srv/v2server.js
@@ -9,22 +9,53 @@ const xsenv = require("@sap/xsenv")
 xsenv.loadEnv();
 const JWTStrategy = require("@sap/xssec").JWTStrategy
 const services = xsenv.getServices({ xsuaa: { tags: "xsuaa" }})
-passport.use(new JWTStrategy(services.xsuaa))
+const xsuaaCredentials = services.xsuaa
+const jwtStrategy = new JWTStrategy(xsuaaCredentials)
+passport.use(jwtStrategy)
 
 // config
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 4004
 const csn = 'gen/csn.json'
 
+// Middleware to read JWT sent by client
+function jwtLogger(req, res, next) {
+  console.log('===> Decoding auth header' )
+  const jwtToken = readJwt(req)
+  if(jwtToken){
+     console.log('===> JWT: scopes: ' + jwtToken.scope)
+     console.log('===> JWT: client_id: ' + jwtToken.client_id)
+     console.log('===> JWT: user: ' + jwtToken.user_name)
+  }
+
+  next()
+}
+
+const readJwt = function(req){
+  const authHeader = req.headers.authorization;
+  if (authHeader){
+     const theJwtToken = authHeader.substring(7);
+     if(theJwtToken){
+        const jwtBase64Encoded = theJwtToken.split('.')[1];
+        if(jwtBase64Encoded){
+           const jwtDecoded = Buffer.from(jwtBase64Encoded, 'base64').toString('ascii');
+           return JSON.parse(jwtDecoded);           
+        }
+     }
+  }
+}
+
 ;(async () => {
   // create new app
   const app = express()
   app.use(helmet())
+  app.use(jwtLogger)
   // Authentication using JWT
   await app.use(passport.initialize())
   await app.use(
     passport.authenticate("JWT", { session: false })
   )
+
   await app.get("/api/userInfo", function (req, res) {
     if(req.user) {
       res.json(req.user)
@@ -33,6 +64,24 @@ const csn = 'gen/csn.json'
     }
   })
 
+  // app endpoint with authorization check does ony work with @sap/xssec > 3.0.0
+  await app.get('/api/getData', function(req, res){
+    console.log('===> Endpoint has been reached. Now checking authorization')
+    const MY_SCOPE = xsuaaCredentials.xsappname + '.admin'// scope name copied from xs-security.json
+    if(req.authInfo.checkScope(MY_SCOPE)){
+      res.send('The endpoint was properly called, role available, delivering data');
+    }else{
+      const jwtToken = readJwt(req)
+      const availableScopes = jwtToken ? jwtToken.scope : {}
+    
+      return res.status(403).json({
+          error: 'Unauthorized',
+          message: `Missing required role: <admin>. Available scopes: ${availableScopes}`
+      });
+    }
+  });
+
+  //  does ony work with @sap/xssec ^2.2.5
   await cds.connect('db') // ensure database is connected!
   // serve odata v4
   await cds
