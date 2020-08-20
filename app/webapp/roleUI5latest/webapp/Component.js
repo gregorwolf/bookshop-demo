6acaf29130f0cc42a01641754d2e37e2c3339b06
Gregor Wolf <gregor.wolf@gmail.com>
2020-08-20 14:56:16 +0200

    Edit Roles with FE on OData V4

---
+++ app/webapp/roleUI5latest/webapp/Component.js
@@ -0,0 +1,10 @@
+sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend('roleUI5latest.Component', {
+        metadata: {
+            manifest: 'json'
+        }
+    });
+});
+
