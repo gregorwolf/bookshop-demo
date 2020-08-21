fe35747b649a5e69b875ec20c79f5df457fddf90
Gregor Wolf <gregor.wolf@gmail.com>
2020-08-21 15:37:55 +0200

    Make Orders V2 and V4 work in UI5 latest

---
+++ app/webapp/ordersUI5latest/webapp/Component.js
@@ -0,0 +1,10 @@
+sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend('ordersUI5latest.Component', {
+        metadata: {
+            manifest: 'json'
+        }
+    });
+});
+
---
+++ app/webapp/v2/ordersUI5latest/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function (
+  AppComponent
+) {
+  return AppComponent.extend('v2.ordersUI5latest.Component', {
+    metadata: {
+      manifest: 'json'
+    }
+  })
+})
