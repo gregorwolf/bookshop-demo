5071c4869f6dcab0d036ccaf7047dee9931e0af5
Gregor Wolf <gregor.wolf@gmail.com>
2022-02-26 21:53:45 +0100

    write mqtt data to persistence

--- srv/mqtt.js
+++ srv/mqtt.js
@@ -1,7 +1,25 @@
+const cds = require("@sap/cds");
+const xsenv = require("@sap/xsenv");
 var mqtt = require("mqtt");
 const { getDestination } = require("@sap-cloud-sdk/core");
 
-getDestination("mqtt").then((mqttDest) => {
+xsenv.loadEnv();
+try {
+  const { mqtt } = xsenv.getServices({ mqtt: { name: "mqtt" } });
+  mqttConnect(mqtt);
+} catch (error) {
+  getDestination("mqtt").then((mqttDest) => {
+    if (mqttDest) {
+      mqttConnect(mqttDest);
+    } else {
+      console.log("[mqtt] can't connect via destination");
+      connectWithUserProvidedService();
+    }
+  });
+}
+
+async function mqttConnect(mqttDest) {
+  const db = await cds.connect.to("db");
   if (mqttDest) {
     var opts = {
       rejectUnauthorized: false,
@@ -32,12 +50,27 @@ getDestination("mqtt").then((mqttDest) => {
       console.log(error.toString());
     });
 
-    client.on("message", function (topic, message) {
+    client.on("message", async function (topic, message) {
       // message is Buffer
-      console.log(topic.toString());
-      console.log(message.toString());
+      // console.log(topic.toString());
+      // console.log(message.toString());
+      let messageJson = JSON.parse(message.toString());
+      if (messageJson.uplink_message?.f_port === 7) {
+        let data = messageJson.uplink_message.decoded_payload;
+        delete data.bytes;
+        delete data.port;
+        data.device_id = messageJson.end_device_ids.device_id;
+        data.received_at = messageJson.received_at;
+        let insert = await db.insert(data).into("iot.Environment");
+        console.log(
+          "inserted " +
+            insert.results[0].affectedRows +
+            " row for device " +
+            data.device_id
+        );
+      }
     });
 
     module.exports = client;
   }
-});
+}
