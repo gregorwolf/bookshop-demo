1459cdf90ef6039a6a28088a144dc7154d8ed66e
Gregor Wolf <gregor.wolf@gmail.com>
2022-03-15 20:29:08 +0100

    enable Approval UI for latest UI5 version

---
+++ app/webapp/v2/approvalUI5latest/Component.js
@@ -0,0 +1,10 @@
+sap.ui.define(
+  ["sap/suite/ui/generic/template/lib/AppComponent"],
+  function (AppComponent) {
+    return AppComponent.extend("v2.approvalUI5latest.Component", {
+      metadata: {
+        manifest: "json",
+      },
+    });
+  }
+);
