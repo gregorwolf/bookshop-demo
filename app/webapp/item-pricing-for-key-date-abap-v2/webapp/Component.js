7d719eba3446e6134d4e70ad208fa3043924b1e7
Gregor Wolf <gregor.wolf@gmail.com>
2022-06-15 13:21:40 +0200

    feat: UI5 app for ABAP CDS View with parameters

---
+++ app/webapp/item-pricing-for-key-date-abap-v2/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
+    return AppComponent.extend("itempricingforkeydataabapv2.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
