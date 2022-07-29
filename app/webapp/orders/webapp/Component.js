915b13763323166defe8296cfab74f9d9ca445d4
Gregor Wolf <gregor.wolf@gmail.com>
2022-07-29 18:00:40 +0200

    update code to app generator output

--- app/webapp/orders/webapp/Component.js
+++ app/webapp/orders/webapp/Component.js
@@ -1,22 +1,9 @@
-sap.ui.define(["sap/fe/AppComponent"], ac => ac.extend("orders.Component", {
-    metadata:{ manifest:'json' }
-}))
+sap.ui.define(["sap/fe/core/AppComponent"], function (Component) {
+  "use strict";
 
-// sap.ui.define (["sap/ui/core/UIComponent"], ui5 => ui5.extend("bookshop.Component", {
-//     metadata: { manifest: "json" }
-// }))
-// sap.ui.define (["sap/ui/generic/app/AppComponent"], ui5 => ui5.extend("bookshop.Component", {
-//     metadata: { manifest: "json" }
-// }))
-
-// jQuery.sap.declare("bookshop.Component");
-// sap.ui.getCore().loadLibrary("sap.ui.generic.app");
-// jQuery.sap.require("sap.ui.generic.app.AppComponent");
-
-// sap.ui.generic.app.AppComponent.extend("bookshop.Component", {
-// 	metadata: {
-// 		manifest: "json"
-// 	}
-// });
-
-/* eslint no-undef:0 */
+  return Component.extend("orders.Component", {
+    metadata: {
+      manifest: "json",
+    },
+  });
+});
