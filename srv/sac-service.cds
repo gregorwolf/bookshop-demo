c207a64f0411ad5285a0d3bbcb1505f3fb17b9ad
Gregor Wolf <gregor.wolf@gmail.com>
2023-08-14 22:43:51 +0200

    working SAC Export requests

--- srv/sac-service.cds
+++ srv/sac-service.cds
@@ -1,6 +1,8 @@
 using {com.sap.cloudDataIntegration as external} from './external/Planned_Events_Sample';
 
 service SacService {
-  function importData() returns String;
-  entity State as projection on external.State_66z5a03o60Master;
+  function importData()  returns String;
+  function getEntities() returns String;
+  function getStates()   returns String;
+  entity States as projection on external.State_66z5a03o60Master;
 }
--- srv/sac-service.js
+++ srv/sac-service.js
@@ -1,10 +1,33 @@
 const cds = require("@sap/cds");
 const LOG = cds.log("sac-service");
+const { executeHttpRequest, getDestination } = require("@sap-cloud-sdk/core");
 
 module.exports = async function (srv) {
   const external = await cds.connect.to("Planned_Events_Sample");
 
-  srv.on("READ", ["State"], async (req) => {
+  srv.on("getEntities", async (req) => {
+    LOG.info("getEntities");
+    const response = await executeHttpRequest(
+      { destinationName: "sac" },
+      {
+        url: "/api/v1/dataexport/providers/sac/sap.epm.Planned_Events_Sample:Planned_Events_Sample/",
+      }
+    );
+    return response.data;
+  });
+
+  srv.on("getStates", async (req) => {
+    LOG.info("getStates");
+    const response = await executeHttpRequest(
+      { destinationName: "sac" },
+      {
+        url: "/api/v1/dataexport/providers/sac/sap.epm.Planned_Events_Sample:Planned_Events_Sample/State_66z5a03o60Master",
+      }
+    );
+    return response.data;
+  });
+
+  srv.on("READ", "States", async (req) => {
     try {
       return await external.run(req.query);
     } catch (error) {
