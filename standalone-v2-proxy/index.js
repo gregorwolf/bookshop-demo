99598903a790a05635fb9e1bf583387db99b2753
Gregor Wolf <gregor.wolf@gmail.com>
2023-10-04 13:46:16 +0200

    switch to @cap-js-community/odata-v2-adapter

--- standalone-v2-proxy/index.js
+++ standalone-v2-proxy/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const express = require("express");
-const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+const cov2ap = require("@cap-js-community/odata-v2-adapter");
 
 const host = "0.0.0.0";
 const port = process.env.PORT || 8080;
@@ -11,17 +11,23 @@ const target = process.env.srv_api_url || "http://localhost:4004";
   const app = express();
 
   // serve odata v2
-  app.use(proxy({
-    path: "v2",
-    target: target,
-    model: "./gen/csn.json",
-    services: {
-      "/Admin": "AdminService",
-      "/Catalog": "CatalogService"
-    }
-  }));
+  app.use(
+    proxy({
+      path: "v2",
+      target: target,
+      model: "./gen/csn.json",
+      services: {
+        "/Admin": "AdminService",
+        "/Catalog": "CatalogService",
+      },
+    })
+  );
 
   // start server
-  const server = app.listen(port, host, () => console.info(`app is listing at ${host}:${port} link for local test: http://localhost:${port}`));
-  server.on("error", error => console.error(error.stack));
+  const server = app.listen(port, host, () =>
+    console.info(
+      `app is listing at ${host}:${port} link for local test: http://localhost:${port}`
+    )
+  );
+  server.on("error", (error) => console.error(error.stack));
 })();
