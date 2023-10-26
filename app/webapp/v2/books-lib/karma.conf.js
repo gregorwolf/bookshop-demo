58718944a5f3201b16c7e88e9658ba7bfae2fb45
Gregor Wolf <gregor.wolf@gmail.com>
2023-10-26 16:11:36 +0200

    add chrome for testing

--- app/webapp/v2/books-lib/karma.conf.js
+++ app/webapp/v2/books-lib/karma.conf.js
@@ -1,36 +1,47 @@
 module.exports = function (config) {
-  'use strict'
+  "use strict";
 
   config.set({
-    frameworks: ['ui5'],
+    frameworks: ["ui5"],
     preprocessors: {
-      'src/**/controls/*.js': ['coverage'],
-      'src/**/libs/*.js': ['coverage']
+      "src/**/controls/*.js": ["coverage"],
+      "src/**/libs/*.js": ["coverage"],
     },
     coverageReporter: {
       includeAllSources: true,
       reporters: [
         {
-          type: 'text'
-        }
+          type: "text",
+        },
       ],
       check: {
         global: {
           statements: 0,
           branches: 0,
           functions: 0,
-          lines: 0
-        }
-      }
+          lines: 0,
+        },
+      },
     },
-    reporters: ['progress', 'coverage'],
+    reporters: ["progress", "coverage"],
 
-    browsers: ['FirefoxHeadless'],
+    browsers: ["FirefoxDebugging"],
 
     browserConsoleLogOptions: {
-      level: 'error'
+      level: "error",
     },
 
-    singleRun: true
-  })
-}
+    customLaunchers: {
+      ChromeDebugging: {
+        base: "Chrome",
+        flags: ["--remote-debugging-port=9333"],
+      },
+      FirefoxDebugging: {
+        base: "FirefoxHeadless",
+        flags: ["--start-debugger-server 9333"],
+      },
+    },
+
+    singleRun: true,
+  });
+};
--- app/webapp/v2/books-lib/test/qunit/controls/GhostButton.qunit.js
+++ app/webapp/v2/books-lib/test/qunit/controls/GhostButton.qunit.js
@@ -1,45 +1,58 @@
 /* global QUnit */
 sap.ui.define(
-    [
-      'sap/ui/qunit/QUnitUtils',
-      'sap/ui/qunit/utils/createAndAppendDiv',
-      'books-lib/controls/GhostButton'
-    ],
-    function (qutils, createAndAppendDiv, GhostButton) {
-      'use strict'
-  
-      // prepare DOM
-      createAndAppendDiv('uiArea1')
-  
-      // module for basic checks
-      QUnit.module('GhostButton Tests')
-  
-      // example sync test
-      QUnit.test('Sync', function (assert) {
-        assert.expect(1)
-        assert.ok(true, 'ok')
-      })
-  
-      // example async test
-      QUnit.test('Async', function (assert) {
-        assert.expect(1)
-        return new Promise(function (resolve, reject) {
-          assert.ok(true, 'ok')
-          resolve()
-        })
-      })
-  
-      // module for basic checks
-      QUnit.module('Basic Control Checks')
-  
-      // some basic control checks
-      QUnit.test('Test get properties', function (assert) {
-        assert.expect(1)
-        const oGhostButton = new GhostButton({
-          text: 'Example'
-        })
-        assert.equal(oGhostButton.getText(), 'Example', "Check text equals 'Example'")
-      })
+  [
+    "sap/ui/qunit/QUnitUtils",
+    "sap/ui/qunit/utils/createAndAppendDiv",
+    "books-lib/controls/GhostButton",
+  ],
+  function (qutils, createAndAppendDiv, GhostButton) {
+    "use strict";
+
+    // prepare DOM
+    createAndAppendDiv("uiArea1");
+
+    // module for basic checks
+    QUnit.module("GhostButton Tests");
+
+    debugger;
+    function sleep(milliseconds) {
+      const date = Date.now();
+      let currentDate = null;
+      do {
+        currentDate = Date.now();
+      } while (currentDate - date < milliseconds);
     }
-  )
-  
+    sleep(10000);
+
+    // example sync test
+    QUnit.test("Sync", function (assert) {
+      assert.expect(1);
+      assert.ok(true, "ok");
+    });
+
+    // example async test
+    QUnit.test("Async", function (assert) {
+      assert.expect(1);
+      return new Promise(function (resolve, reject) {
+        assert.ok(true, "ok");
+        resolve();
+      });
+    });
+
+    // module for basic checks
+    QUnit.module("Basic Control Checks");
+
+    // some basic control checks
+    QUnit.test("Test get properties", function (assert) {
+      assert.expect(1);
+      const oGhostButton = new GhostButton({
+        text: "Example",
+      });
+      assert.equal(
+        oGhostButton.getText(),
+        "Example",
+        "Check text equals 'Example'"
+      );
+    });
+  }
+);
