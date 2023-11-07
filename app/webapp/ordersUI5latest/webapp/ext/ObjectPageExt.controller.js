f7e67dbee73a7fa72b69eacdaefe8b331e22d33c
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-11-07 15:50:10 +0100

    only use macros rte

--- app/webapp/ordersUI5latest/webapp/ext/ObjectPageExt.controller.js
+++ app/webapp/ordersUI5latest/webapp/ext/ObjectPageExt.controller.js
@@ -2,66 +2,27 @@ sap.ui.define(
   ['sap/ui/core/mvc/ControllerExtension'],
   function (ControllerExtension) {
     'use strict';
-    return ControllerExtension.extend('ordersUI5latest.ext.ListReportExt', {
+    return ControllerExtension.extend('ordersUI5latest.ext.ObjectPageExt', {
+
       override: {
-        routing: {
-          onBeforeBinding: function (oBindingContext, mParameters) {
-            this.initRichTextEditor();
-          },
-          onAfterBinding: function (oBindingContext, mParameters) {
-            console.log('onAfterBinding: Data received');
+        onInit: function () {
+          console.log('onAfterRendering');
+        },
+        editFlow: {
+          onAfterEdit: function (oBindingContext, mParameters) {
             this.getView().byId("myRichTextEditor").attachReady(this.RTELoaded, this);
           },
+
         },
+        pageReady:{
+          onPageReady: function (oBindingContext, mParameters) {
+            console.log('onPageReady');
+          }
+        }
       },
       RTELoaded: function (oEvent) {
         this.getView().byId("myRichTextEditor").addPlugin("autoresize");
-      },
-      initRichTextEditor: function (bIsTinyMCE5) {
-        var that = this,
-          sHtmlValue =""
-        sap.ui.require(
-          [
-            'sap/ui/richtexteditor/RichTextEditor',
-            'sap/ui/richtexteditor/library',
-          ],
-          function (RTE, library) {
-            var EditorType = library.EditorType;
-            that.oRichTextEditor = new RTE('myRTE', {
-              editorType: EditorType.TinyMCE6,
-              width: '100%',
-              height: '600px',
-              customToolbar: false,
-              // showGroupFont: true,
-              // showGroupLink: true,
-              // showGroupInsert: true,
-              plugins: [{
-                name: "fullscreen"
-              }],
-              value: sHtmlValue,
-              ready: function () {
-                this.addButtonGroup('styles').addButtonGroup('table');
-                this.addButtonGroup({
-                  name: "fullscreen",
-                  visible: true,
-                  priority: 10,
-                  customToolbarPriority: 10,
-                  buttons: [
-                    "fullscreen"
-                  ]
-                });
-              },
-            });
-
-            that
-              .getView()
-              .byId(
-                'ordersUI5latest::OrdersObjectPage--fe::CustomSubSection::customSection2--idVerticalLayout'
-              )
-              .addContent(that.oRichTextEditor);
-          }
-        );
-      },
+      }
     });
   }
 );
