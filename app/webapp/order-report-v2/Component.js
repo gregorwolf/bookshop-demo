f4c51b5f662c5008ac883516b531c693f3949648
Gregor Wolf <gregor.wolf@gmail.com>
2024-08-07 17:17:27 +0200

    OData V2 UI for parameterized view

---
+++ app/webapp/order-report-v2/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("orderreportv2.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
