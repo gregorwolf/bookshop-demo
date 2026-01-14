940024d476c68860c41b87fe73a4baab5831365f
Gregor Wolf <gregor.wolf@gmail.com>
2026-01-14 17:46:57 +0100

    add V2 extension

--- app/webapp/v2/ordersUI5latest/ext/controller/ListReportExtension.controller.js
+++ app/webapp/v2/ordersUI5latest/ext/controller/ListReportExtension.controller.js
@@ -1,21 +1,21 @@
-sap.ui.controller("v2.orders.ext.controller.ListReportExtension", {
-  onInit: function () { 
-    this.getView().byId("listReport").setProperty("useExportToExcel", true) 
+sap.ui.controller("v2.ordersUI5latest.ext.controller.ListReportExtension", {
+  onInit: function () {
+    this.getView().byId("listReport").setProperty("useExportToExcel", true);
   },
-  adaptTransientMessageExtension:function() {
+  adaptTransientMessageExtension: function () {
     var messages = sap.ui.getCore().getMessageManager().getMessageModel().oData;
-    if(messages.length) {
-        var message = messages[0];
-        if(message.code === "OSS001") {
-          var msgText = "This message has beed added through List Extension" ;
-          var consolidatedMessage = new sap.ui.core.message.Message({
-            message: message.message,
-            type: message.type,
-            target: '',
-            persistent: true
-          });
-          sap.ui.getCore().getMessageManager().addMessages(consolidatedMessage);  
-        }
+    if (messages.length) {
+      var message = messages[0];
+      if (message.code === "OSS001") {
+        var msgText = "This message has been added through List Extension";
+        var consolidatedMessage = new sap.ui.core.message.Message({
+          message: message.message,
+          type: message.type,
+          target: "",
+          persistent: true,
+        });
+        sap.ui.getCore().getMessageManager().addMessages(consolidatedMessage);
+      }
     }
-  }
+  },
 });
